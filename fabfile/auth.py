8edde1d3e9845aaefba25f586ab5c1965c903ff9
Takaaki Kurihara <32270422+kuritaka@users.noreply.github.com>
2018-02-05 01:32:26 +0900

    organize login user

--- fabfile/auth.py
+++ fabfile/auth.py
@@ -2,34 +2,77 @@ import sys, glob, os
 from fabric.api import *
 from fabric.contrib import files
 
+
+#------------------------------------------------------------------------
+# Linux
+#------------------------------------------------------------------------
 @task
 def pro():
-    '''production env'''
+    '''Production # USER=fasthandle KEY'''
     env.user = 'fasthandle'
     env.key_filename = '$FHHOME/key/id_rsa.fasthandle.pro-fasthandle-1'
     #env.password = 'passphrase-for-key'
 
 @task
 def stg():
-    '''staging env'''
+    '''Staging # USER=fasthandle KEY'''
     env.user = 'fasthandle'
     env.key_filename = '$FHHOME/key/id_rsa.fasthandle.stg-fasthandle-1'
     #env.password = 'passphrase-for-key'
 
 @task
 def dev():
-    '''develop env'''
+    '''Develop # USER=fasthandle'''
     env.user = 'fasthandle'
     env.key_filename = '$FHHOME/key/id_rsa.fasthandle.dev-fasthandle-1'
     #env.password = 'passphrase-for-key'
 
 @task
 def test1():
+    '''USER=fasthandle PASSWORD=fas***s'''
     env.user = 'fasthandle'
     env.password = 'fastpass'
 
 @task
 def test2():
+    '''USER=user01 PASSWORD=us***01'''
     env.user = 'user01'
     env.password = 'user01'
+
+#------------------------------------------------------------------------
+# Windows
+#------------------------------------------------------------------------
+@task
+def winpro():
+    '''USER=fasthandle KEY'''
+    env.user = 'fasthandle'
+    env.key_filename = '$FHHOME/key/id_rsa.fasthandle.pro-fasthandle-1'
+
+@task
+def winstg():
+    '''USER=fasthandle PASSWORD=fas***s'''
+    env.user = 'fasthandle'
+    env.password = 'fastpass'
+
+#------------------------------------------------------------------------
+# Hardware
+#------------------------------------------------------------------------
+@task
+def ciscopro():
+    '''USER=fasthandle PASSWORD=fas***s'''
+    env.user = 'fasthandle'
+    env.password = 'fastpass'
+
+@task
+def junospro():
+    '''USER=fasthandle PASSWORD=fas***s'''
+    env.user = 'fasthandle'
+    env.password = 'fastpass'
+
+@task
+def netapppro():
+    '''USER=root PASSWORD=fas***s'''
+    env.user = 'root'
+    env.password = 'fastpass'
+
 
