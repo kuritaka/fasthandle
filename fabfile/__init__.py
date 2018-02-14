1bf5eebd07e37b3f8f43f7dca7a2a1a5cbdc7cf7
Takaaki Kurihara <32270422+kuritaka@users.noreply.github.com>
2018-02-15 01:36:22 +0900

    Operation History to log file

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,20 +1,36 @@
 import sys, select
 from fabric.api import *
 from fabric.contrib import files
-   
+from datetime import datetime
+
 env.warn_only = True
 env.port = 22
 env.eagerly_disconnect = True
- 
- 
+
+
+# standard in for example echo x.x.x.x
 if select.select([sys.stdin,],[],[],0.0)[0]:
     lines = sys.stdin.read().splitlines()
     env.hosts = filter(bool, lines)
- 
- 
+
+
+#FastHandle Operation History to log file
+TIME = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+yymm = datetime.now().strftime('%Y%m')
+
+f=open("%s/log/history.%s.log" % (FHHOME,yymm) ,"a")
+f.write("%s " % TIME)
+f.write(' '.join(str(p) for p in sys.argv))
+f.write("\n")
+f.close()
+
+
+#-------------------------------------------
+# import 
+#-------------------------------------------
 import auth
 import test
- 
+
 ## OS Management
 import user
 import pkg
@@ -23,13 +39,12 @@ import set
 import get
 import check
 import ope
- 
+
 ## Middleware Management
 import httpd
 import nginx
 import postfix
 import squid
-import tftp
- 
+
 ## Programing Languages
 import php
