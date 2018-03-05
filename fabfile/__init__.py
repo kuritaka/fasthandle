e268491e01fc29add9f78f88a6f8154beb4a01ef
Takaaki Kurihara <32270422+kuritaka@users.noreply.github.com>
2018-03-06 01:24:34 +0900

    auto import *.py

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -4,18 +4,26 @@ from fabric.contrib import files
 from datetime import datetime
 
 
+#======================================================
+# Variable
+#======================================================
 FHHOME=os.environ["FHHOME"]
 
 env.warn_only = True
 env.port = 22
 env.eagerly_disconnect = True
 
+#don't create *.pyc without __init__.pyc
+sys.dont_write_bytecode = True
 
+
+
+#======================================================
+#======================================================
 # standard in for example echo x.x.x.x
 if select.select([sys.stdin,],[],[],0.0)[0]:
     lines = sys.stdin.read().splitlines()
     env.hosts = filter(bool, lines)
-
 
 
 #FastHandle Operation History to log file
@@ -36,26 +44,22 @@ for p in env.hosts:
 f.close()
 
 
-#-------------------------------------------
-# import 
-#-------------------------------------------
-import auth
-import test
+# auto import *.py
+# Ref. https://gist.github.com/fereria/3331554f4c480679716b#file-__init__-py
+pathThisFile = os.path.dirname(os.path.abspath(__file__))
 
-## OS Management
-import user
-import pkg
-import net
-import set
-import get
-import check
-import ope
+def loadModule():
 
-## Middleware Management
-import httpd
-import nginx
-import postfix
-import squid
+    myself = sys.modules[__name__]
 
-## Programing Languages
-import php
+    #print __name__
+
+    mod_paths = glob.glob(os.path.join(pathThisFile, '*.py'))
+    for py_file in mod_paths:
+        mod_name = os.path.splitext(os.path.basename(py_file))[0]
+        if re.search(".*__init__.*",mod_name) is None:
+            mod = importlib.import_module(__name__+ "." + mod_name)
+            #for m in mod.__dict__.keys():
+                #if not m in ['__builtins__', '__doc__', '__file__', '__name__', '__package__']:
+                    #myself.__dict__[m] = mod.__dict__[m]
+loadModule()
