a42cf9d4566338c38fa0d926c53d986e7aa21ebc
Takaaki Kurihara <32270422+kuritaka@users.noreply.github.com>
2018-05-08 00:53:39 +0900

    update test.py

--- fabfile/test.py
+++ fabfile/test.py
@@ -1,6 +1,7 @@
 import sys, os
 from fabric.api import *
 from fabric.contrib import files
+from datetime import datetime
 
 FHHOME=os.environ["FHHOME"]
 
@@ -15,8 +16,44 @@ def hostname():
     run('hostname')
 
 
-# test.cat_etc_shadow
+# test.sudo
 @task
-def cat_etc_shadow():
+def sudo():
+    """ sudo cat /etc/shadow """
     sudo('cat /etc/shadow')
+
+
+# test.arg1:hostname
+@task
+def arg1(cmd):
+    """ fab test.arg1:hostname """
+    run('%s' % cmd)
+
+
+# test.arg2:hostname,whoami
+@task
+def arg2(cmd1, cmd2):
+    """ fab test.arg2:hostname,whoami """
+    run('%s' % cmd1)
+    run('%s' % cmd2)
+    run('%s ; %s' % (cmd1, cmd2) )
+
+
+# test.put
+@task
+def put():
+    date = datetime.now().strftime('%Y%m%d_%H%M')
+    local('hostname > /tmp/test.%s' % date)
+    put('/tmp/test.%s' % date, '/tmp/test.%s' % date)
+    run('ls -lh /tmp/test.%s' % date)
+    run('cat /tmp/test.%s' % date)
+
+# test.get
+@task
+def get():
+    date = datetime.now().strftime('%Y%m%d_%H%M')
+    run('hostname > /tmp/test.%s' % date)
+    get('/tmp/test.%s' % date, '/tmp/test.%s' % date)
+    local('ls -lh /tmp/test.%s' % date)
+    local('cat /tmp/test.%s' % date)
 
